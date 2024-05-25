dc1e50f740feca6d41b7c8c6fde671c682df6d8b
suyash101101 <143525373+suyash101101@users.noreply.github.com>
2024-05-25 05:58:48 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,61 @@
+const STORE = [
+    {
+      question: 'In a website browser address bar, what does “www” stand for? ',
+      answers: ['World Wide What', 'World Wide Web', 'World Wine Web', 'World White Web'],
+      correctAnswer: 'World Wide Web',
+    },
+    {
+      question: 'What is the tiny piece at the end of a shoelace called?',
+      answers: ['lace tip', 'shoe tip', 'aglet', 'lace'],
+      correctAnswer: 'aglet',
+    },
+    {
+      question: 'Originally, Amazon only sold what kind of product?',
+      answers: ['Phones', 'Electronics', 'Home appliances', 'Books'],
+      correctAnswer: 'Books',
+    },
+    {
+      question: 'Who was the first woman to win a Nobel Prize?',
+      answers: ['marie curie', 'mother terresa', 'helen keller', 'shakuntala devi'],
+      correctAnswer: 'marie curie',
+    },
+    {
+      question: 'In what year was the internet opened to the public?',
+      answers: ['1993', '1992', '1991', '1990'],
+      correctAnswer: '1993',
+    },
+    {
+      question: 'When did Facebook first launch?"',
+      answers: ['2005', 
+      '2004',
+      '2006', 
+      '2008'],
+      correctAnswer: '2004',
+    },
+    {
+      question: 'What skin color did Gene Roddenberry initially imagine for Spock?',
+      answers: ['Red', 'Blue', 'Purple', 'Green'],
+      correctAnswer: 'Red',
+    },
+    {
+      question: 'How many total episodes were there?(star trek)',
+      answers: ['48', '67', '79', '110'],
+      correctAnswer: '79',
+    },
+    {
+      question: 'How old was Rose in the Titanic movie when she was recounting her story',
+      answers: ['60', '75', 
+      '85', '100'],
+      correctAnswer: '100',
+    },
+    {
+      question: 'How many Harry Potter books and movies are there?',
+      answers: ['7,8', '8,7', '7,7', '8,8'],
+      correctAnswer: '7,7',
+    },
+    {
+      question: 'Who invented the Klingon language?',
+      answers: ['James Doohan', 'Leonard Nimoy', 'George Takei', 'Walter Koenig'],
+      correctAnswer: 'James Doohan',
+    },  
+  ];
